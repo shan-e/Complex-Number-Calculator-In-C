6d1cd08905a4b75ee391a2a409261dcd61ac454c
shan-e <126803195+shan-e@users.noreply.github.com>
2024-05-08 15:34:16 +0100

    Update funcs.h
    comment show that it is function prototypes

--- funcs.h
+++ funcs.h
@@ -1,7 +1,7 @@
 #ifndef FUNCS_H
 #define FUNCS_H
 
-
+// function prototypes
 struct Complex { double realPart; double imaginaryPart; };
 void printUsage(char *executable);
 struct Complex addComplex(struct Complex num1, struct Complex num2);
