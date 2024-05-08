eadff0669780cadd0bf5352ce15d57e216341804
shan-e <126803195+shan-e@users.noreply.github.com>
2024-05-08 15:33:39 +0100

    Update funcs.c
    describe code workings with comments

--- funcs.c
+++ funcs.c
@@ -2,6 +2,15 @@
 #include <stdlib.h>
 #include "funcs.h"
 
+/*
+    struct Complex operateComplex(struct Complex num1, struct Complex num2) {
+        create variable to hold final result
+        operate real parts of num1 and num2 together
+        operate imaginary parts of num1 and num2 together
+        return result as type Complex
+    }
+*/
+
 struct Complex addComplex(struct Complex num1, struct Complex num2) {
     struct Complex result;
     result.realPart = num1.realPart + num2.realPart;
@@ -31,4 +40,5 @@ struct Complex divideComplex(struct Complex num1, struct Complex num2) {
     return result;
 }
 
+// print instructions on how to use command line args 
 void printUsage(char *executable) { printf("Usage: %s <realNum1> <imaginaryNum1> <+ | - | * | /> <realNum2> <imaginaryNum2> in format -> z = a + bi \n", executable); exit(-1); }
